2d89c56b19af2f07bdf6bccd1ec20566eb25f9bc
Siddhant Rele <siddhantrele@gmail.com>
2020-11-17 14:30:42 -0500

    Update detect.py

--- trainedmodel/detect.py
+++ trainedmodel/detect.py
@@ -27,7 +27,7 @@ def detectPoints(folderName):
     parser = argparse.ArgumentParser()
     parser.add_argument("--image_folder", type=str, default="./static/uploads/"+folderName+'/test', help="path to dataset")
     parser.add_argument("--model_def", type=str, default="./trainedmodel/config/yolov3-custom.cfg", help="path to model definition file")
-    parser.add_argument("--weights_path", type=str, default="./trainedmodel/checkpoints/yolov3_ckpt_70_most_optimal.pth", help="path to weights file")
+    parser.add_argument("--weights_path", type=str, default="./trainedmodel/checkpoints/yolov3_ckpt_100.pth", help="path to weights file")
     parser.add_argument("--class_path", type=str, default="./trainedmodel/data/custom/classes.names", help="path to class label file")
     parser.add_argument("--conf_thres", type=float, default=0.8, help="object confidence threshold")
     parser.add_argument("--nms_thres", type=float, default=0.4, help="iou thresshold for non-maximum suppression")
