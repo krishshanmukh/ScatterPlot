805861ce4ec2cd1f2de35d643f262bfafb275e98
Sri Krishna Shanmukh <smedapati@cs.stonybrook.edu>
2020-11-29 16:00:57 -0500

    [Data Scaling] Used column values to reconstruct data at original scale
    There were values in columns tab that were left unused.
    Used data from them to reconstruct data at original scale.
    
    Signed-off-by: Sri Krishna Shanmukh <smedapati@cs.stonybrook.edu>

--- trainedmodel/detect.py
+++ trainedmodel/detect.py
@@ -25,14 +25,14 @@ from matplotlib.ticker import NullLocator
 
 PREDICTED_FOLDER = os.path.join('static', 'predicted')
 
-def detectPoints(folderName):
+def detectPoints(folderName, column_values):
     parser = argparse.ArgumentParser()
     parser.add_argument("--image_folder", type=str, default="./static/uploads/"+folderName+'/test', help="path to dataset")
     parser.add_argument("--model_def", type=str, default="./trainedmodel/config/yolov3-custom.cfg", help="path to model definition file")
     parser.add_argument("--weights_path", type=str, default="./trainedmodel/checkpoints/yolov3_ckpt_100.pth", help="path to weights file")
     parser.add_argument("--class_path", type=str, default="./trainedmodel/data/custom/classes.names", help="path to class label file")
     parser.add_argument("--conf_thres", type=float, default=0.8, help="object confidence threshold")
-    parser.add_argument("--nms_thres", type=float, default=0.4, help="iou thresshold for non-maximum suppression")
+    parser.add_argument("--nms_thres", type=float, default=0, help="iou thresshold for non-maximum suppression")
     parser.add_argument("--batch_size", type=int, default=1, help="size of the batches")
     parser.add_argument("--n_cpu", type=int, default=0, help="number of cpu threads to use during batch generation")
     parser.add_argument("--img_size", type=int, default=416, help="size of each image dimension")
@@ -132,15 +132,25 @@ def detectPoints(folderName):
                 box_w = x2 - x1
                 box_h = y2 - y1
 
-                point_x_cord = (x1 + x2)/2
-                point_y_cord = img.shape[1] - (y1 + y2)/2
+                # print(img.shape)
+                if x1<0 or x2<0 or y1<0 or y2<0 or x1 >img.shape[1] or x2>img.shape[1] or \
+                y1 > img.shape[0] or y2 > img.shape[0]:
+                    # print(x1, x2, y1, y2, "dsfs")
+                    continue
+                
+                y_scale = (column_values[img_i+1][1] - column_values[img_i+1][0])
+                y_limit = column_values[img_i+1][0]
+                x_scale = column_values[0][1] - column_values[0][0]
+                x_limit = column_values[0][0]
+                point_x_cord = (x1 + x2)/2 * (x_scale) / img.shape[1] + x_limit
+                point_y_cord = (img.shape[1] - (y1 + y2)/2)*y_scale/img.shape[0] + y_limit
                 # print(point_x_cord, point_y_cord, conf, cls_conf, cls_pred)
                 imgPointsDict[img_i+1].append((point_x_cord.item(),point_y_cord.item(), box_w, box_h))
 
                 color = bbox_colors[int(np.where(unique_labels == int(cls_pred))[0])]
                 #if not classes[int(cls_pred)] == "labels":
                 # Create a Rectangle patch
-                bbox = patches.Rectangle((x1, y1), box_w, box_h, linewidth=0.5, edgecolor=color, facecolor="none")
+                bbox = patches.Rectangle((x1, y1), box_w, box_h, linewidth=2, edgecolor=color, facecolor="none")
                   # Add the bbox to the plot
                 ax.add_patch(bbox)
                 # Add label
@@ -158,6 +168,7 @@ def detectPoints(folderName):
         plt.axis("off")
         plt.gca().xaxis.set_major_locator(NullLocator())
         plt.gca().yaxis.set_major_locator(NullLocator())
+        print(path)
         filename = path.split("/")[-1].split(".")[0]
         plt.savefig(os.path.join(fname, filename+".jpg"), bbox_inches="tight", pad_inches=0.0)
         print(os.path.join(fname, filename+".jpg"))
